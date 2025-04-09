8b6db9b567aa6771be92e4d2c5fe926ec6294c75
Everton Lopes <everton.lopes@zero-defect.com.br>
2025-04-09 17:27:14 -0300

    COISA

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import pprint
 import os
-import cassiopeia as lol
+import requests
+import cassiopeia as cass
+from cassiopeia import Champion, Champions
+# Use any translator you like, in this example GoogleTranslator
+
+
+def get_champion(champion: str):
+    champion = champion.lower().strip().capitalize()
+    champions_obj = requests.get(f"https://ddragon.leagueoflegends.com/cdn/15.7.1/data/pt_BR/champion/{champion}.json").json()
+    champions_obj = champions_obj["data"][champion]
+    print(champions_obj["recommended"])
+    
+if __name__ == "__main__":
+    champions = Champions(region="NA")
+    for champion in champions:
+        try:
+            print(f"\n{champion.name}\n")
+            get_champion(champion.name)
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
